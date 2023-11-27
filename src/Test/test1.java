d1c4d3a4c73cff08846150da3b4f95b6ff9ec782
sonamgawas <sonam.gawas99@gmail.com>
2023-11-27 10:52:15 +0530

    tc-2 and tc-3

--- src/Test/test1.java
+++ src/Test/test1.java
@@ -6,6 +6,8 @@ public class test1 {
 		// TODO Auto-generated method stub
 System.out.println("github command");
 System.out.println("new code");
+System.out.println("test case 1");
+System.out.println("tc-2");
 	}
 
 }
